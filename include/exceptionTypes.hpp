decfed7287c3abb8f2c3de57ba5337e19291fc27
Cédric Honorat <chonorat@student.42lyon.fr>
2025-04-16 16:38:07 +0200

    feat(exception): add new error type for file opening failure and improve exception messages

--- include/exceptionTypes.hpp
+++ include/exceptionTypes.hpp
@@ -3,12 +3,14 @@
 
 #include <exception>
 #include <string>
+#include <utility>
 
 enum errorType {
-  	NO_ERROR,				//0
-  	NO_ARG_ERROR,			//1
-	TOO_MANY_ARG_ERROR,		//2
-  	WRONG_EXTENSION_ERROR	//3
+  	NO_ERROR,				    //0
+  	NO_ARG_ERROR,			    //1
+	TOO_MANY_ARG_ERROR,		    //2
+  	WRONG_EXTENSION_ERROR,	    //3
+	UNABLE_TO_OPEN_FILE_ERROR	//4
 };
 
 extern errorType errorCode;
@@ -17,29 +19,36 @@ class BaseException : public std::exception {
   protected:
     	std::string message;
   public:
-      	explicit BaseException(const std::string& message) : message(message) {}
+      	explicit BaseException(std::string  message) : message(std::move(message)) {}
       	const char* what() const _NOEXCEPT override { return message.c_str(); }
 };
 
 class NoArgException : public BaseException {
   public: 
-    	NoArgException() : BaseException("ERROR: Insert a .obj file name to load\nUSAGE: ./scop file.obj\n") {
+    	NoArgException() : BaseException("ERROR: Insert a .obj file name to load\nUSAGE: ./scop file.obj") {
           		errorCode = NO_ARG_ERROR;
     	}
 };
 
 class TooManyArgException : public BaseException {
   public: 
-		TooManyArgException() : BaseException("ERROR: Too many arguments\nUSAGE: ./scop file.obj\n") {
+		TooManyArgException() : BaseException("ERROR: Too many arguments\nUSAGE: ./scop file.obj") {
                   errorCode = TOO_MANY_ARG_ERROR;
 		}
 };
 
 class WrongExtensionException : public BaseException {
   public: 
-		WrongExtensionException() : BaseException("ERROR: Wrong file extension\nUSAGE: ./scop file.obj\n") {
+		WrongExtensionException() : BaseException("ERROR: Wrong file extension\nUSAGE: ./scop file.obj") {
 				  errorCode = WRONG_EXTENSION_ERROR;
 		}
+};
+
+class UnableToOpenFileException : public BaseException {
+  public: 
+        UnableToOpenFileException() : BaseException("ERROR: Unable to open file") {
+                  errorCode = UNABLE_TO_OPEN_FILE_ERROR;
+        }
 };
 
 #endif //EXCEPTIONTYPES_HPP
