81b1a4e53f76ff3175ff4f747deb92f3ce7fee32
Cédric Honorat <chonorat@student.42lyon.fr>
2025-04-16 16:35:02 +0200

    feat(vectors): add Vec2 and Vec3 classes for 2D and 3D vector representation

---
+++ include/vectors.hpp
@@ -0,0 +1,19 @@
+#ifndef VECTORS_HPP
+#define VECTORS_HPP
+
+class Vec3 {
+	public:
+		float x, y, z;
+        Vec3() : x(0), y(0), z(0) {}
+		Vec3(const float x, const float y, const float z) : x(x), y(y), z(z) {}
+};
+
+class Vec2 {
+	public:
+		float u, v;
+		Vec2() : u(0), v(0) {}
+		Vec2(const float u, const float v) : u(u), v(v) {}
+};
+
+
+#endif //VECTORS_HPP
