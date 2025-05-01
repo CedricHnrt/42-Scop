95dae6e9312eab2ba568ddcf0a70fd162ab982e9
Cédric Honorat <chonorat@student.42lyon.fr>
2025-05-01 20:50:46 +0200

    refactor(vectors): comment out unused Vec2 class to improve code clarity

--- include/vectors.hpp
+++ include/vectors.hpp
@@ -8,12 +8,11 @@ class Vec3 {
 		Vec3(const float x, const float y, const float z) : x(x), y(y), z(z) {}
 };
 
-class Vec2 {
-	public:
-		float u, v;
-		Vec2() : u(0), v(0) {}
-		Vec2(const float u, const float v) : u(u), v(v) {}
-};
-
+//class Vec2 {
+//	public:
+//		float u, v;
+//		Vec2() : u(0), v(0) {}
+//		Vec2(const float u, const float v) : u(u), v(v) {}
+//};
 
 #endif //VECTORS_HPP
