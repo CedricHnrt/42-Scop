734f33976aa17ad52fe91faeb58031b2a0f4559a
Cédric Honorat <chonorat@student.42lyon.fr>
2025-05-01 18:42:00 +0200

    feat(ansiCodes): add ANSI color codes and text formatting definitions

---
+++ include/ansiCodes.hpp
@@ -0,0 +1,10 @@
+#ifndef ANSICODES_HPP
+#define ANSICODES_HPP
+
+#define RESET       "\033[0m"
+#define BOLD        "\033[1m"
+
+#define RED         "\033[31m"
+#define GREEN       "\033[32m"
+
+#endif //ANSICODES_HPP
