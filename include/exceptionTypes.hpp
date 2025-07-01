8e6e3dbb38fa2d8bd389accc45fb6a30abb752da
chonorat <chonorat@student.42lyon.fr>
2025-07-01 16:38:07 +0200

    feat(Exception): add RuntimeError type and refactor exception classes

--- include/exceptionTypes.hpp
+++ include/exceptionTypes.hpp
@@ -11,45 +11,53 @@ enum errorType {
   	NO_ARG_ERROR,			    //2
 	TOO_MANY_ARG_ERROR,		    //3
   	WRONG_EXTENSION_ERROR,	    //4
-	UNABLE_TO_OPEN_FILE_ERROR	//5
+	UNABLE_TO_OPEN_FILE_ERROR,	//5
+	RUNTIME_ERROR				//6
 };
 
 extern errorType errorCode;
 
 class BaseException : public std::exception {
-  protected:
-    	std::string message;
-  public:
-      	explicit BaseException(std::string message) : message(std::move(message)) {}
-      	const char* what() const noexcept override { return message.c_str(); }
+	protected:
+   		std::string message;
+	public:
+     		explicit BaseException(std::string message) : message(std::move(message)) {}
+     		const char* what() const noexcept override { return message.c_str(); }
 };
 
 class NoArgException final : public BaseException {
-  public: 
-    	NoArgException() : BaseException("ERROR: Insert a .obj file name to load\nUSAGE: ./scop file.obj") {
+	public: 
+   		NoArgException() : BaseException("ERROR: Insert a .obj file name to load\nUSAGE: ./scop file.obj") {
           		errorCode = NO_ARG_ERROR;
-    	}
+   		}
 };
 
 class TooManyArgException final : public BaseException {
-  public: 
+	public: 
 		TooManyArgException() : BaseException("ERROR: Too many arguments\nUSAGE: ./scop file.obj") {
-                  errorCode = TOO_MANY_ARG_ERROR;
+                		errorCode = TOO_MANY_ARG_ERROR;
 		}
 };
 
 class WrongExtensionException final : public BaseException {
-  public: 
+	public: 
 		WrongExtensionException() : BaseException("ERROR: Wrong file extension\nUSAGE: ./scop file.obj") {
-				  errorCode = WRONG_EXTENSION_ERROR;
+						errorCode = WRONG_EXTENSION_ERROR;
 		}
 };
 
 class UnableToOpenFileException final : public BaseException {
-  public: 
-        UnableToOpenFileException() : BaseException("ERROR: Unable to open file") {
-                  errorCode = UNABLE_TO_OPEN_FILE_ERROR;
-        }
+	public: 
+	     UnableToOpenFileException() : BaseException("ERROR: Unable to open file") {
+	               errorCode = UNABLE_TO_OPEN_FILE_ERROR;
+	     }
+};
+
+class RuntimeException final : public BaseException {
+	public:
+		explicit RuntimeException(const std::string &message) : BaseException(message) {
+			errorCode = RUNTIME_ERROR;
+		}
 };
 
 #endif //EXCEPTIONTYPES_HPP
